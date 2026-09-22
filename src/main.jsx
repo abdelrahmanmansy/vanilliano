700ac9ba056097ff017874c4387a88cc8f0e0f51
Abdelrahman A. Mansy <abdelrahmanahmedmansy@gmail.com>
2026-09-22 12:22:24 +0300

    Add basename to BrowserRouter for GitHub Pages subpath

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { ProductsProvider } from './context/ProductsContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <ProductsProvider>
           <CartProvider>
